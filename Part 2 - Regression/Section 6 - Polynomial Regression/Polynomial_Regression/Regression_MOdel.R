32c6dbe42ceba58731c19527b21fa24ae33d4dba
ps2802 <praneet.sinha28@gmail.com>
2017-08-31 15:21:09 -0400

    Init Commit

---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/Regression_MOdel.R
@@ -0,0 +1,39 @@
+# Importing the Dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Slipting the Dataset into the Training Set and Test Set
+#install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# # Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+
+# Fitting  Regression to the dataset
+# Create your regressor here
+
+
+# Predciting a new Result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+
+# Visualizing the  Regression Model( for higher resolution and smoother curve)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid$Level, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
+
+
